6314482f365c8c00f595894876462f278f11c17d
Bruna <brunabenne@gmail.com>
2023-10-12 14:30:56 -0300

    Função que mostra quantas notas foram menores que 6

--- src/testeGit/TesteGit.java
+++ src/testeGit/TesteGit.java
@@ -9,19 +9,25 @@ public class TesteGit {
 		
 		int[] notas = new int [5];
 		int soma = 0;
+		int menorQueSeis = 0;
 		
 		for(int i = 0; i < 5; i++) {		
 			System.out.println("Nota aluno " + (i+1) + ": ");
 			notas[i] = sc.nextInt();
 			soma += notas[i];
+			if(notas[i]>6) {
+				menorQueSeis += 1;
+			}
 		}
 		
 		int media = soma/5;
 		
+	
+		
 		System.out.println("As notas são: " + Arrays.toString(notas));
 		System.out.println("Soma das notas: " + soma);
 		System.out.println("Média das notas: " + media);
-		
+		System.out.println("Quantas notas menor que 6: " + menorQueSeis);
 		
 		
 		sc.close();
